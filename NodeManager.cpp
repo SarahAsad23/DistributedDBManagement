01a11019a01dc16726c613649ab63a13d0aabc56
Sarah Asad <sarah_asad@live.com>
2024-11-19 16:16:07 -0800

    DB tables and node manager created

---
+++ NodeManager.cpp
@@ -0,0 +1,26 @@
+/*
+* This file will contain the methods to read and write to the database 
+*/
+
+#include <iostream>
+#include <cstdio>
+
+
+using namespace std;
+
+class NodeManager{
+    // This will help us find which row we want to operate on (read/write)
+    int NodeManager::GetRecord(){
+        
+    }
+
+    // reading from a node/file
+    string NodeManager::Read(){
+    
+    }
+
+    // writitng to a node/file 
+    void NodeManager::Write(){
+
+    }
+};
